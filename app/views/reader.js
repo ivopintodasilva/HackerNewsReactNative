49675a1044267418e6c0db117f55d7779afc6dac
Ivo Silva <ivopintodasilva@gmail.com>
2016-12-20 14:45:37 +0000

    Activity indicator implemented in reader view

--- app/views/reader.js
+++ app/views/reader.js
@@ -8,22 +8,74 @@ import {
   Alert,
   TabBarIOS,
   WebView,
+  ActivityIndicator
 } from 'react-native';
 
 let styles = StyleSheet.create({
+    activity_indicator: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+    },
     container: {
         flex: 1,
-        backgroundColor: "#000000",
     },
 });
 
 export class Reader extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true
+        }
+    }
+
+    loadStarted = () => {
+        this.setState({
+            loading: true
+        });
+    }
+
+    loadEnded = () => {
+        this.setState({
+            loading: false
+        });
+    }
+
     render() {
-        return(
-            <WebView
-                source={{uri: this.props.url}}
-                style={styles.container}
-            />
-        );
+
+        switch (this.state.loading) {
+            case false:
+                return(
+                    <View style={{flex: 1}}>
+                        <WebView
+                        onLoadStart={this.loadStarted}
+                        onLoadEnd={this.loadEnded}
+                        source={{uri: this.props.url}}
+                        style={styles.container}
+                        />
+                    </View>
+                );
+            case true:
+                return(
+                    <View style={{flex: 1}}>
+                        <WebView
+                        onLoadStart={this.loadStarted}
+                        onLoadEnd={this.loadEnded}
+                        source={{uri: this.props.url}}
+                        style={styles.container}
+                        />
+                        
+                        <ActivityIndicator
+                        style={styles.activity_indicator}
+                        size='small'
+                        />
+                    </View>
+                );
+        }
     }
 }
